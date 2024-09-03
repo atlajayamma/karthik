5d2cd23b8fd78b974b03c12aa557796bfd8d731a
atlajayamma <atlapallavi42@gmail.com>
2024-09-03 12:22:12 +0530

    titanic histogram

--- p14.py
+++ p14.py
@@ -2,11 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 data=pd.read_csv('D:\\Dataset\\titanic.csv')
 print(data)
-##Histogram
 a_s=data[data['survived']==1]['age']
 an_s=data[data['survived']==0]['age']
-plt.hist(a_s,color='green',alpha=0.5,label="survived")
-plt.hist(an_s,color='purple',alpha=1,label="not survived")
+plt.hist(a_s,color='orange',alpha=0.5,label="survived")
+plt.hist(an_s,color='pink',alpha=1,label="not survived")
 plt.xlabel('age')
 plt.ylabel('frequency')
 plt.title('age distribution of survived and not survived pasengers')
