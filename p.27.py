d918fff9cf8d85bc513562382e63287594bc67ad
atlajayamma <atlapallavi42@gmail.com>
2024-10-02 18:02:52 +0530

    ki

--- p.27.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df=pd.read_csv(r"C:\Users\PALLAVI ATLA\OneDrive\\Documents\Dataset 1\\Dataset\\titanic.csv")
-#Find the total number of missing values from each column.
-print(df.isnull().sum())
-
